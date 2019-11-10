6212079d25383393cb08bb04ad9e9875e2389281
acheerio <acheerioo@gmail.com>
2019-11-10 00:03:24 -0600

    Modularizing

--- projects/oherina/dominion/unittest1.c
+++ projects/oherina/dominion/unittest1.c
@@ -13,14 +13,19 @@ void testBaron() {
 	struct gameState G;
 	initializeGame(numPlayer, k, seed, &G); // initialize a new game
 	
+	// calling tests and passing by value since we want gameState unchanged
+	baronTest1(G);
+}
+
+void baronTest1(struct gameState state) {
 	printf("Baron - choose to discard estate, estate is first card.\n");
 	// set up state - currentPlayer 0 with hand: estate, baron
-	G.whoseTurn = 0;
-	G.coins = 2;
-	int coinsExpected = G.coins + 4;
-	G.handCount[G.whoseTurn] = 2;
-	G.hand[G.whoseTurn][0] = estate;
-	G.hand[G.whoseTurn][1] = baron;
+	state.whoseTurn = 0;
+	state.coins = 2;
+	int coinsExpected = state.coins + 4;
+	state.handCount[state.whoseTurn] = 2;
+	state.hand[state.whoseTurn][0] = estate;
+	state.hand[state.whoseTurn][1] = baron;
 	
 	int card = baron;
 	int choice1 = 1; // choose to discard estate
@@ -32,15 +37,15 @@ void testBaron() {
 	assert("Function returns 0 (Success)", result, 0);
 	
 	printf("2) Baron card moved out of player's hand.\n");
-	assert("Baron removed from hand.", hasCard(G.whoseTurn, baron, G), FALSE);
-	assert("Baron placed at top of played cards.", G.playedCards[G.playedCardCount], baron);
+	assert("Baron removed from hand.", hasCard(state.whoseTurn, baron, G), FALSE);
+	assert("Baron placed at top of played cards.", state.playedCards[state.playedCardCount], baron);
 	
 	printf("3) Estate card discarded.\n");
-	assert("Estate card removed from hand.", hasCard(G.whoseTurn, estate, G), FALSE);
-	assert("Estate card discarded.", G.discard[G.whoseTurn][G.discardCount[G.whoseTurn]], estate);
+	assert("Estate card removed from hand.", hasCard(state.whoseTurn, estate, G), FALSE);
+	assert("Estate card at top of discard pile.", state.discard[state.whoseTurn][state.discardCount[state.whoseTurn]], estate);
 	
 	printf("4) Four coins gained.\n");
-	assert("+4 Coins", G.coins, coinsExpected);
+	assert("+4 Coins", state.coins, coinsExpected);
 }
 
 int main() {
