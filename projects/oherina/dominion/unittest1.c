253fd0fc92cb017214a85e83c895041f6cbd67bf
acheerio <acheerioo@gmail.com>
2019-11-09 23:11:02 -0600

    unittest1 baron v2

--- projects/oherina/dominion/unittest1.c
+++ projects/oherina/dominion/unittest1.c
@@ -1,12 +1,12 @@
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include "cardEffect.h"
+#include "assert.h"
 #include <stdlib.h>
 #include <stdio.h>
 // unittest1
 
 void testBaron() {
-	int i;
 	int seed = 1; // need some constant here that's deterministic (vs seeding by time)
 	int numPlayer = 2;
 	int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
@@ -18,9 +18,9 @@ void testBaron() {
 	G.whoseTurn = 0;
 	G.coins = 2;
 	int coinsExpected = G.coins + 4;
-	G.handCount[currentPlayer] = 2;
-	G.hand[currentPlayer][0] = estate;
-	G.hand[currentPlayer][1] = baron;
+	G.handCount[G.whoseTurn] = 2;
+	G.hand[G.whoseTurn][0] = estate;
+	G.hand[G.whoseTurn][1] = baron;
 	
 	int card = baron;
 	int choice1 = 1; // choose to discard estate
@@ -32,10 +32,10 @@ void testBaron() {
 	assert("   Function returns 0 (Success)\n", result, 0);
 	printf("Baron card moved out of player's hand.\n");
 	assert("   Baron removed from hand.\n", hasCard(G.whoseTurn, baron, G), FALSE);
-	assert("   Baron placed at top of played cards.\n", state->playedCards[state->playedCardCount], baron);
+	assert("   Baron placed at top of played cards.\n", G.playedCards[G.playedCardCount], baron);
 	printf("Estate card discarded.\n");
 	assert("   Estate card removed from hand.\n", hasCard(G.whoseTurn, estate, G), FALSE);
-	assert("   Estate card discarded.\n", discard[G.whoseTurn][discardCount[G.whoseTurn]], estate);
+	assert("   Estate card discarded.\n", G.discard[G.whoseTurn][G.discardCount[G.whoseTurn]], estate);
 	printf("Four coins gained.\n");
 	assert("   +4 Coins", G.coins, coinsExpected);
 }
