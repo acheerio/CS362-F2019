88cb4573a4d48c5b63702b2fb5318ee5926a9954
acheerio <acheerioo@gmail.com>
2019-11-10 21:47:02 -0600

    fixed problem with assert.h

--- projects/oherina/dominion/assert.h
+++ projects/oherina/dominion/assert.h
@@ -7,7 +7,7 @@ typedef enum bool {FALSE, TRUE} bool;
 
 bool assert(char * assertion, int a, int b);
 bool hasCard(int player, int card, struct gameState state);
-
+int getCount(int player, int card, struct gameState state);
 // fills the player's hand with specified number of card (overwrites previous values)
 void fillHand(int player, int card, int count, struct gameState *state);
 
