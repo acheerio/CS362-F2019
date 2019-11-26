87f078b9f9eac7cd27b7e243ecab24eef37a20bc
acheerio <acheerioo@gmail.com>
2019-11-25 21:58:51 -0600

    Bug 6 test

--- projects/FinalProject-Bugs/dominion/unittest-bug6.c
+++ projects/FinalProject-Bugs/dominion/unittest-bug6.c
@@ -14,5 +14,20 @@ int main () {
     memset(&G, 23, sizeof(struct gameState)); // set the game state
     initializeGame(numPlayers, k, seed, &G); // initialize a new game
 
+    printf("Begin Bug #6 unit test: Play feast to gain gold (cost = 6)\n");
+
+    int currentPlayer = 0;
+    G.handCount[currentPlayer] = 2;
+    G.hand[currentPlayer][0] = feast;
+    G.hand[currentPlayer][1] = silver; // +2 coins
+    G.discardCount[currentPlayer] = 0;
+    int bonus = 0;
+
+    int result = cardEffect(feast, gold, -1, -1, &G, 0, &bonus);
+
+    assertPrint("Function unsuccessful.", result, -1);
+    assertPrint("Gold not taken from supply.", G.supplyCount[gold], 30);
+    assertPrint("Gold not gained to discard.", G.discardCount[currentPlayer], 0);
+
     return 0;
 }
